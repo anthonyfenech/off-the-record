9460a3e209c59d9ffd557d3a9b73aaef5e467b65
Anthony Fenech <fenech@Anthonys-MacBook-Air.local>
2026-01-06 18:39:39 -0500

    Chapter 3 DREAM JOB: Full content with italics and formatting

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Service Worker for OFF-THE-RECORD
 
-const CACHE_VERSION = 'v122';
-const STATIC_CACHE = 'off-the-record-static-v122';
-const CONTENT_CACHE = 'off-the-record-content-v122';
+const CACHE_VERSION = 'v123';
+const STATIC_CACHE = 'off-the-record-static-v123';
+const CONTENT_CACHE = 'off-the-record-content-v123';
 
 // Files to cache immediately on install
 const STATIC_ASSETS = [
