da39b2be3be5c56161f25d39121221552f8887a5
Anthony Fenech <fenech@Anthonys-MacBook-Air.local>
2026-01-06 18:24:16 -0500

    Update paragraph indent to 0.5 inches, no indent after datelines/section breaks

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Service Worker for OFF-THE-RECORD
 
-const CACHE_VERSION = 'v119';
-const STATIC_CACHE = 'off-the-record-static-v119';
-const CONTENT_CACHE = 'off-the-record-content-v119';
+const CACHE_VERSION = 'v120';
+const STATIC_CACHE = 'off-the-record-static-v120';
+const CONTENT_CACHE = 'off-the-record-content-v120';
 
 // Files to cache immediately on install
 const STATIC_ASSETS = [
