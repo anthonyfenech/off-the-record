7f13b7d9ae62fc5d2a423c683676bb53a27f688d
Anthony Fenech <fenech@Anthonys-MacBook-Air.local>
2026-01-07 18:15:56 -0500

    Update PWA icons - new 180/192/512 icons, simplified manifest

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Service Worker for OFF-THE-RECORD
 
-const CACHE_VERSION = 'v194';
-const STATIC_CACHE = 'off-the-record-static-v194';
-const CONTENT_CACHE = 'off-the-record-content-v194';
+const CACHE_VERSION = 'v195';
+const STATIC_CACHE = 'off-the-record-static-v195';
+const CONTENT_CACHE = 'off-the-record-content-v195';
 
 // Files to cache immediately on install
 const STATIC_ASSETS = [
