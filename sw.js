d5ccbe2f94440b6df829fbd0533c6ca65745fa4e
Anthony Fenech <fenech@Anthonys-MacBook-Air.local>
2026-01-06 19:49:13 -0500

    Fix header vertical centering with explicit height

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Service Worker for OFF-THE-RECORD
 
-const CACHE_VERSION = 'v127';
-const STATIC_CACHE = 'off-the-record-static-v127';
-const CONTENT_CACHE = 'off-the-record-content-v127';
+const CACHE_VERSION = 'v128';
+const STATIC_CACHE = 'off-the-record-static-v128';
+const CONTENT_CACHE = 'off-the-record-content-v128';
 
 // Files to cache immediately on install
 const STATIC_ASSETS = [
