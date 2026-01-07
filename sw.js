92e090d1afb34d046b35945c74609adf34cbb09f
Anthony Fenech <fenech@Anthonys-MacBook-Air.local>
2026-01-07 01:43:30 -0500

    Hide nav footer on home page

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Service Worker for OFF-THE-RECORD
 
-const CACHE_VERSION = 'v153';
-const STATIC_CACHE = 'off-the-record-static-v153';
-const CONTENT_CACHE = 'off-the-record-content-v153';
+const CACHE_VERSION = 'v154';
+const STATIC_CACHE = 'off-the-record-static-v154';
+const CONTENT_CACHE = 'off-the-record-content-v154';
 
 // Files to cache immediately on install
 const STATIC_ASSETS = [
