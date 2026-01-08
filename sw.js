6460a4e0d09351a714c673a9cb7d88718bebc7dd
Anthony Fenech <fenech@Anthonys-MacBook-Air.local>
2026-01-07 20:49:33 -0500

    Move icons to assets/icons folder, update references

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Service Worker for OFF-THE-RECORD
 
-const CACHE_VERSION = 'v216';
-const STATIC_CACHE = 'off-the-record-static-v216';
-const CONTENT_CACHE = 'off-the-record-content-v216';
+const CACHE_VERSION = 'v217';
+const STATIC_CACHE = 'off-the-record-static-v217';
+const CONTENT_CACHE = 'off-the-record-content-v217';
 
 // Files to cache immediately on install
 const STATIC_ASSETS = [
