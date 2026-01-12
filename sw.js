a164ea8e1bec23a42ba855e423002d165a85dac3
Anthony Fenech <fenech@Anthonys-MacBook-Air.local>
2026-01-11 19:04:08 -0500

    Close button alignment -16px - v230

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Service Worker for OFF-THE-RECORD
 
-const CACHE_VERSION = 'v229';
-const STATIC_CACHE = 'off-the-record-static-v229';
-const CONTENT_CACHE = 'off-the-record-content-v229';
+const CACHE_VERSION = 'v230';
+const STATIC_CACHE = 'off-the-record-static-v230';
+const CONTENT_CACHE = 'off-the-record-content-v230';
 
 // Files to cache immediately on install
 const STATIC_ASSETS = [
