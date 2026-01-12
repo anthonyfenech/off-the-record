6bfc2a386ef9eb23843da20ab2aba09aa29229bf
Anthony Fenech <fenech@Anthonys-MacBook-Air.local>
2026-01-11 22:49:24 -0500

    Revert TOC and parental advisory positions

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Service Worker for OFF-THE-RECORD
 
-const CACHE_VERSION = 'v249';
-const STATIC_CACHE = 'off-the-record-static-v249';
-const CONTENT_CACHE = 'off-the-record-content-v249';
+const CACHE_VERSION = 'v250';
+const STATIC_CACHE = 'off-the-record-static-v250';
+const CONTENT_CACHE = 'off-the-record-content-v250';
 
 // Files to cache immediately on install
 const STATIC_ASSETS = [
