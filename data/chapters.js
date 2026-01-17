5364a3f066a52fd87a013b61e5e2736c968a4ee7
Anthony Fenech <fenech@Anthonys-MacBook-Air.local>
2026-01-17 04:21:14 -0500

    Add avila-verlander-trade photo after eleven seconds

--- data/chapters.js
+++ data/chapters.js
@@ -3700,6 +3700,9 @@ export const CHAPTERS = [
             <p>"Yes, the deal is done."</p>
             <p>"Thank you," and nothing more.</p>
             <p>The call lasts eleven seconds. I click here and WIN.</p>
+
+<p class="media-embed"><span class="media-emoji" data-media-id="avila-verlander-trade">📷</span></p>
+
             <p>My name crawls across the bright red breaking news bar on the TV screen. According to Anthony Fenech.</p>
 
             <p>&nbsp;</p>
