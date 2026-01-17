f94a92931b9b68b2b263c1cdbd9a3afcd8a00563
Anthony Fenech <fenech@Anthonys-MacBook-Air.local>
2026-01-17 18:53:56 -0500

    Speed up modal transitions (300ms -> 150ms) for snappier popups

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Service Worker for OFF-THE-RECORD
 
-const CACHE_VERSION = 'v307';
-const STATIC_CACHE = 'off-the-record-static-v307';
-const CONTENT_CACHE = 'off-the-record-content-v307';
+const CACHE_VERSION = 'v308';
+const STATIC_CACHE = 'off-the-record-static-v308';
+const CONTENT_CACHE = 'off-the-record-content-v308';
 
 // Files to cache immediately on install
 const STATIC_ASSETS = [
