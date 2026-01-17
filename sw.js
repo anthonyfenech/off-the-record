54133d6fd13b568182c6e3c4168a6d68d691929d
Anthony Fenech <fenech@Anthonys-MacBook-Air.local>
2026-01-17 18:31:05 -0500

    Bump cache version to v304 to clear stale homepage cache

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Service Worker for OFF-THE-RECORD
 
-const CACHE_VERSION = 'v303';
-const STATIC_CACHE = 'off-the-record-static-v303';
-const CONTENT_CACHE = 'off-the-record-content-v303';
+const CACHE_VERSION = 'v304';
+const STATIC_CACHE = 'off-the-record-static-v304';
+const CONTENT_CACHE = 'off-the-record-content-v304';
 
 // Files to cache immediately on install
 const STATIC_ASSETS = [
